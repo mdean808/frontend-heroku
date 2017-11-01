5f6ea08ee31ceba2fa25e0a95e555ed61d6d1114
Unicorn <unicorncalcs@gmail.com>
2017-11-01 08:29:15 -1000

    Fixed up project structure

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 const express = require('express'), app = express();
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public_html'));
 app.listen(process.env.PORT || 8080);
