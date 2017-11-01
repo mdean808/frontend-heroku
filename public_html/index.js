bcdaf5aa2a499c94e4655fb2a2ee77d5b0d1535a
mdean808 <mdean@hpa.edu>
2017-11-01 09:25:16 -1000

    created function loadProfile() to load profile from an exmaple json in sesrver.js

--- public_html/index.js
+++ public_html/index.js
@@ -17,6 +17,22 @@ function loadProfile() {
 		},
 		success: function (results) {
 			console.log(results);
+			$('#name').val('John');
+			var projectAmount = results.projectAmount;
+			for(var i = 0; i < projectAmount; i++) {
+				$('#projects').append('<div class="card horizontal">\n' +
+					'        <div class="card-image center" style="padding-left: 10px; margin-top: 10px;">\n' +
+					'            <img width="86" height="86" src="https://maxcdn.icons8.com/Share/icon/Logos//google_logo1600.png">\n' +
+					'            <h6>Student Corner</h6>\n' +
+					'        </div>\n' +
+					'        <div class="card-stacked">\n' +
+					'            <div class="card-content">\n' +
+					'                <p>This is an short, but helpful project description. </p>\n' +
+					'                <a href="#" style="margin-top: 40px;" class="btn blue waves-effect">Go</a>\n' +
+					'            </div>\n' +
+					'        </div>\n' +
+					'    </div>')
+			}
 		},
 		error: function (result, err) {
 			console.log(err);
@@ -24,7 +40,6 @@ function loadProfile() {
 		}
 	})
 }
-
 
 var getQueryString = function ( field, url ) {
 	var href = url ? url : window.location.href;
--- server.js
+++ server.js
@@ -1,16 +1,56 @@
 const express = require('express'), app = express();
 const bodyParser = require('body-parser');
 app.use(express.static(__dirname + '/public_html'));
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8081);
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+var exampleJson = '{\n' +
+	'  "profiles": [\n' +
+	'    {\n' +
+	'      "id" : "0",\n' +
+	'      "firstName": "John",\n' +
+	'      "lastName": "Doe",\n' +
+	'      "gradYear": "2210",\n' +
+	'      "advisor": "John Doe 2",\n' +
+	'      "projects": [\n' +
+	'        {\n' +
+	'          "id": "1",\n' +
+	'          "name": "The best project",\n' +
+	'          "desc": "This is a great description"\n' +
+	'        },\n' +
+	'        {\n' +
+	'          "id": "2",\n' +
+	'          "name": "The best project",\n' +
+	'          "desc": "This is a great description"\n' +
+	'        }      ]\n' +
+	'    },\n' +
+	'    {\n' +
+	'      "id" : "1",\n' +
+	'      "firstName": "John",\n' +
+	'      "lastName": "Doe",\n' +
+	'      "gradYear": "2210",\n' +
+	'      "advisor": "John Doe 2",\n' +
+	'      "projects": {\n' +
+	'        "project": {\n' +
+	'          "id": "1",\n' +
+	'          "name": "The best project",\n' +
+	'          "desc": "This is a great description"\n' +
+	'        }\n' +
+	'      }\n' +
+	'    }\n' +
+	'  ]\n' +
+	'}\n';
+
+exampleJson = JSON.parse(exampleJson);
 app.post('/get-userinfo', function (req, res) {
 	var id = req.body.id;
 	console.log(id);
 	res.writeHead(200, {'Content-Type': 'application/json'});
 	res.end(JSON.stringify({
-		user: id
+		user: exampleJson.profiles[0].id,
+		projectAmount: exampleJson.profiles[0].projects.length,
+		projectsJSON: exampleJson.profiles[0].projects
 	}));
 });
