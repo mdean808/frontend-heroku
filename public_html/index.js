5a46b92004cd9d6870d7b5fdafdea9fe3003d34f
mdean808 <mdean@hpa.edu>
2017-11-01 08:59:56 -1000

    created way to send info from db

--- public_html/index.js
+++ public_html/index.js
@@ -7,3 +7,28 @@ function logIn() {
 function loadLogin() {
 
 }
+
+function loadProfile() {
+    $.ajax({
+		method: 'post',
+		url: '/get-userinfo',
+		data: {
+			user: getQueryString('id')
+		},
+		success: function (results) {
+			console.log(results);
+		},
+		error: function (result, err) {
+			console.log(err);
+			console.log(result);
+		}
+	})
+}
+
+
+var getQueryString = function ( field, url ) {
+	var href = url ? url : window.location.href;
+	var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
+	var string = reg.exec(href);
+	return string ? string[1] : null;
+};
--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const express = require('express'), app = express();
+const bodyParser = require('body-parser');
 app.use(express.static(__dirname + '/public_html'));
 app.listen(process.env.PORT || 8080);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.post('/get-userinfo', function (req, res) {
+	var id = req.body.id;
+	console.log(id);
+	res.writeHead(200, {'Content-Type': 'application/json'});
+	res.end(JSON.stringify({
+		user: id
+	}));
+});
